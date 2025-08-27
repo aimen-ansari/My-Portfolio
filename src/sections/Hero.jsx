cb788fcb3b61f551016828b546f4e0e21a9e3157
aimen-ansari <aimenansari4@gmail.com>
2025-08-27 21:56:40 +0500

    initial

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -60,7 +60,7 @@ const Hero = () => {
 
             <Button
               text="Click to Scroll"
-              className="md:w-80 md:h-16 w-80 h-12"
+              className="md:w-80 md:h-16 w-70 h-12"
               id="button"
             />
           </div>
